adea7029c4f3168809e8e348e3cb6083f158f6b3
Anubhavgupta14 <anubhavgu2002@gmail.com>
2023-07-27 02:23:18 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,96 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import { TaskService } from './services/task.service';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CoreService } from './core/core.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'myApp';
+export class AppComponent implements OnInit {
+  title = 'Task Management';
+
+  displayedColumns: string[] = [
+    'id',
+    'Task',
+    'Duedate',
+    'Description',
+    'Priority',
+    'Status',
+    'action',
+  ];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private _dialog: MatDialog, private _empService: TaskService, private _coreService: CoreService) {}
+
+  ngOnInit(): void {
+    this.getTaskList();
+  }
+
+  openAddEdittaskForm() {
+    const dialogRef = this._dialog.open(EmpAddEditComponent);
+    dialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if (val) {
+          this.getTaskList();
+        }
+      },
+    });
+  }
+
+  getTaskList() {
+    this._empService.getTaskList().subscribe({
+      next: (res) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  deleteTask(id: number) {
+    this._empService.deleteTask(id).subscribe({
+      next: (res) => {
+        this._coreService.openSnackBar('Task Deleted','done')
+        this.getTaskList();
+      },
+      error: console.log,
+    });
+  }
+
+  openEditForm(data: any) {
+    const dialogRef = this._dialog.open(EmpAddEditComponent, {
+      data,
+    });
+
+    dialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if (val) {
+          this.getTaskList();
+        }
+      },
+    });
+  }
 }
---
+++ src/app/emp-add-edit/emp-add-edit.component.ts
@@ -0,0 +1,63 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { TaskService } from '../services/task.service';
+import { DialogRef } from '@angular/cdk/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CoreService } from '../core/core.service';
+@Component({
+  selector: 'app-emp-add-edit',
+  templateUrl: './emp-add-edit.component.html',
+  styleUrls: ['./emp-add-edit.component.scss'],
+})
+export class EmpAddEditComponent implements OnInit {
+  empForm: FormGroup;
+  education: string[] = ['To-Do', 'In-progress', 'Completed'];
+
+  constructor(
+    private _fb: FormBuilder,
+    private _empService: TaskService,
+    private _dialogRef: MatDialogRef<EmpAddEditComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private _coreService: CoreService
+  ) {
+    this.empForm = this._fb.group({
+      Task: '',
+      Duedate: '',
+      Description: '',
+      Priority: '',
+      Status: '',
+    });
+  }
+
+  ngOnInit(): void {
+    this.empForm.patchValue(this.data);
+  }
+
+  onFormSubmit() {
+    if (this.empForm.valid) {
+      if (this.data) {
+        this._empService
+          .updateTask(this.data.id, this.empForm.value)
+          .subscribe({
+            next: (val: any) => {
+              this._coreService.openSnackBar('Task updated successfully','done')
+              this._dialogRef.close(true);
+            },
+            error: (err: any) => {
+              console.error(err);
+            },
+          });
+      } else {
+        this._empService.addTask(this.empForm.value).subscribe({
+          next: (val: any) => {
+            this._coreService.openSnackBar('Task added successfully','done');
+            this._dialogRef.close(true);
+          },
+          error: (err: any) => {
+            console.error(err);
+          },
+        });
+      }
+    }
+  }
+}
